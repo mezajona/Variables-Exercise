a1f78b8d05854a6863e3f2256dd1bb6ee3194798
Jonathan Meza <jonathansyear92@gmail.com>
2025-01-03 11:38:01 -0800

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace VariablesExercise
+﻿using System.Threading.Channels;
+
+namespace VariablesExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Jonathan";
+            int age = 31;
+            double avg = 108.7;
+            decimal bankAccount = 108.72m;
+            char initial = 'S';
+            bool isMarried = true;
+
+            //Console.WriteLine($"Name: {myName}\nAge: {age}\nAvg. Account: {avg}\nMoney: {bankAccount}\nInitial: {initial}\nIsMarried: {isMarried}");
+            Console.WriteLine($"My name is {myName}, I am {age} years old. I spent {bankAccount} dollars on dinner last night with my wife celebrating our anniversary.");
         }
     }
 }
